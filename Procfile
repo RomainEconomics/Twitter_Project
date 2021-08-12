d4dcf1c9c7d9776a282eb19185b4c2088d8a9f7b
RomainEconomics <jouhameau.romain@gmail.com>
2021-08-12 11:55:40 +0200

    change procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && streamlit run Streamlit_App/streamlit_app.py
+web: sh setup.sh && streamlit run Streamlit_App/app.py
--- Streamlit_App/apps/model.py
+++ Streamlit_App/apps/model.py
@@ -24,7 +24,6 @@ from sklearn.decomposition import NMF
 
 # Librairie pour stocker les différents modèles 
 import joblib
-#plt.use( 'tkagg' )
 
 def app():
 
